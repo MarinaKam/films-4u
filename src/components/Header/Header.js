dbbe167f8eeeb17fd72a2f26faedcee61df05850
Marina Kam <kam.marvic@gmail.com>
2019-03-28 13:56:05 +0200

    added onBlur function for safari and moz browser with ref

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,40 @@
-import React  from 'react';
+import React, { Component }  from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import SearchBar from "../SearchBar/SearchBar";
-// import headerImg from '../../img/header.jpg';
 
-const Header = ({onClick, onBlur}) => (
-    <header className={styles.header}>
-        <div className={styles['header-overlay']}></div>
-        <Link to="/" className={styles['header-logo']}>Films4U</Link>
-        <div className={styles['header-search']}>
-            <button
-                className={styles['header-menu']}
-                onClick={onClick}
-                onBlur={onBlur}
-            >
-                <i className="fas fa-bars fa-2x"></i>
-            </button>
-            <SearchBar/>
-        </div>
-        <div className={styles['header-auth']}>
-            <button className={styles['header-auth__btn']}>Log In</button>
-        </div>
-    </header>
-);
+class Header extends Component {
+
+    menuHandleClick = e => {
+        e.preventDefault();
+        this.menuBtn.focus();
+        this.props.onClick();
+    };
+
+    render() {
+        const { onBlur } = this.props;
+        return (
+            <header className={styles.header}>
+                <div className={styles['header-overlay']}></div>
+                <Link to="/" className={styles['header-logo']}>Films4U</Link>
+                <div className={styles['header-search']}>
+                    <button
+                        ref={ button => this.menuBtn = button }
+                        className={styles['header-menu']}
+                        onClick={this.menuHandleClick}
+                        onBlur={onBlur}
+                    >
+                        <i className="fas fa-bars fa-2x"></i>
+                    </button>
+                    <SearchBar/>
+                </div>
+                <div className={styles['header-auth']}>
+                    <button className={styles['header-auth__btn']}>Log In</button>
+                </div>
+            </header>
+        );
+    }
+}
 
 
 export default Header;
