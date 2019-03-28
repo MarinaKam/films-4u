b3bd89dc9a1af564affa1b6e79a4d8066a652fcc
Marina Kam <kam.marvic@gmail.com>
2019-03-28 09:25:03 +0200

    changd some styles in search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,20 +4,21 @@ import styles from './SearchBar.module.css';
 class SearchBar extends Component {
 
     state = {
-        value: ''
+        searchTerm: ''
     };
 
     handleChange = e =>  this.setState({
-        value: e.target.value
+        searchTerm: e.target.value
     });
 
     handleSubmit = e => {
         e.preventDefault();
-        this.setState({value: ''});
-        console.log(this.state.value);
+        this.setState({searchTerm: ''});
+        console.log(this.state.searchTerm);
     };
 
     render() {
+        const { searchTerm } = this.state;
         return(
             <form
               onSubmit={this.handleSubmit}
@@ -29,11 +30,12 @@ class SearchBar extends Component {
                   className={styles['search-btn']}><i className="fas fa-search fa-sm"></i>
                 </button>
                 <input
-                  onChange={this.handleChange}
-                  value={this.state.value}
                   className={styles['search-field']}
                   type="search"
-                  placeholder='Search video...'/>
+                  placeholder='Search video...'
+                  onChange={this.handleChange}
+                  value={ searchTerm }
+                />
             </form>
         );
     }
