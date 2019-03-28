c674856a45e42c9c30f11baccf4eb0a4c73fe42f
Marina Kam <kam.marvic@gmail.com>
2019-03-28 22:55:38 +0200

    added conditions to the searchresult component

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -2,22 +2,23 @@ import React  from 'react';
 import List from "../List/List";
 
 const SearchResult = ({searchResults, query}) => {
-        return (
-            <div>
-                <h1>Search result</h1>
-                   {searchResults ?
-                    <div>
-                         <p>There are <b>{searchResults.length}</b>  results for: "{ query }".</p>
-                         <List list={ searchResults }/>
-                     </div>
-                     :
-                     <div>
-                         <p>¯\_(ツ)_/¯</p>
-                         <p>There are no movies, try again.</p>
-                     </div>
-                 }
-            </div>
-        );
+    if(!searchResults) return <div>Loading...</div>;
+    return (
+        <div>
+            <h1>Search result</h1>
+               {searchResults.length > 0 ?
+                <div>
+                     <p>There are <b>{searchResults.length}</b>  results for: "{ query }".</p>
+                     <List list={ searchResults }/>
+                 </div>
+                 :
+                 <div style={{textAlign: 'center', fontSize: '26px'}}>
+                     <p style={{fontSize: '56px', color: 'red'}}>¯\_(ツ)_/¯</p>
+                     <p>There are no movies, try again.</p>
+                 </div>
+             }
+        </div>
+    );
 };
 
 export default SearchResult;
