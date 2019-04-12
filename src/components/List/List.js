0c8c4f2b96231a2dcaa002d7bc8db2c2b0f9ba5e
Marina Kam <kam.marvic@gmail.com>
2019-04-12 18:37:15 +0300

    changed styles for empty holder in the list component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,62 +1,45 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './List.module.css';
-// import MovieItem from "../MovieItem/MovieItem";
 
-class List extends Component {
-    state = {
-        height: 299
-    };
-
-    componentDidMount() {
-        this.calcHeight();
-        window.addEventListener('resize', this.calcHeight);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.calcHeight);
-    }
-
-    calcHeight = () => this.setState({height: this.refHolder.clientHeight});
-
-    render() {
-        const { list } = this.props;
-        const { height } = this.state;
-        const item = list.map(({title, id, vote_average, poster_path}) => (
-            <div key={id} className={styles['list-item']}>
-                <span className={styles['list-item__average']}>{vote_average}</span>
-                <div
-                    ref={holder => this.refHolder = holder}
-                    className={styles['list-item__img']}>
-                    { poster_path === null ?
-                        <Link to={`/movie/${id}`} style={{height: height}}>
-                            <div className={styles['list-item__holder']}></div>
-                        </Link>
-                        :
-                        <Link to={`/movie/${id}`}>
-                            <img
-                                alt={title}
-                                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${poster_path}`}
-                            />
-                        </Link>
-                    }
-                    <div className={styles['list-img__overlay']}>
-                        <div className={styles['list-img__overlay-txt']}>
-                            <button><i className="fas fa-heart"></i></button>
-                            <Link to={`/movie/${id}`}><i className="fas fa-play"></i></Link>
+const List = ({ list }) => {
+    const item = list.map(({title, id, vote_average, poster_path}) => (
+        <div key={id} className={styles['list-item']}>
+            <span className={styles['list-item__average']}>{vote_average}</span>
+            <div className={styles['list-item__img']}>
+                { poster_path === null ?
+                    <Link
+                        className={`${styles['list-item__link']} ${styles['list-item__empt-link']}`}
+                        to={`/movie/${id}`}>
+                        <div className={styles['list-item__holder']}>
+                            <p>"No image available"</p>
                         </div>
+                    </Link>
+                    :
+                    <Link
+                        className={styles['list-item__link']}
+                        to={`/movie/${id}`}>
+                        <img
+                            alt={title}
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${poster_path}`}
+                        />
+                    </Link>
+                }
+                <div className={styles['list-img__overlay']}>
+                    <div className={styles['list-img__overlay-txt']}>
+                        <button><i className="fas fa-heart"></i></button>
+                        <Link to={`/movie/${id}`}><i className="fas fa-play"></i></Link>
                     </div>
                 </div>
-                <div className={styles['list-item__title']}>{title}</div>
             </div>
-        ));
-        return(
-                <div className={styles['list']}>
-                    { item }
-                </div>
-            );
-    }
-
-}
+            <div className={styles['list-item__title']}>{title}</div>
+        </div>
+    ));
+    return(
+            <div className={styles['list']}>
+                { item }
+            </div>
+        );
+};
 
 export default List;
